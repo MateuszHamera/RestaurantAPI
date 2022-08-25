a9c22a658fea45ed1c3bfd087b085d2dbc503359
Mateusz <mateusz.hamera@evatronix.com>
2022-08-25 21:10:33 +0200

    Added Get, GetAll and Delete to DishController.

--- RestaurantAPI/Controllers/DishController.cs
+++ RestaurantAPI/Controllers/DishController.cs
@@ -14,12 +14,32 @@ namespace RestaurantAPI.Controllers
             _dishService = dishService;
         }
 
+        [HttpGet("{dishId}")]
+        public ActionResult<DishDto> GetById([FromRoute]int restaurantId, [FromRoute]int dishId)
+        {
+            return Ok(_dishService.GetById(restaurantId, dishId));
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<DishDto>> GetAll([FromRoute] int restaurantId)
+        {
+            return Ok(_dishService.GetAll(restaurantId));
+        }
+
         [HttpPost]
         public ActionResult Create([FromRoute]int restaurantId, [FromBody]DishDto dishDto)
         {
             var id = _dishService.Create(restaurantId, dishDto);
 
             return Created($"/api/{restaurantId}/dish/{id}", null);
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteAll([FromRoute] int restaurantId)
+        {
+            _dishService.RemoveAll(restaurantId);
+
+            return NoContent();
         }
     }
 }
--- RestaurantAPI/Services/DishService.cs
+++ RestaurantAPI/Services/DishService.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RestaurantAPI.Entities;
 using RestaurantAPI.Exceptions;
 using RestaurantAPI.Models;
@@ -8,6 +10,9 @@ namespace RestaurantAPI.Services
     public interface IDishService
     {
         int Create(int restaurantId, DishDto dishDto);
+        DishDto GetById(int restaurantId, int dishId);
+        IEnumerable<DishDto> GetAll(int restaurantId);
+        void RemoveAll(int restaurantId);
     }
     public class DishService : IDishService
     {
@@ -19,14 +24,34 @@ namespace RestaurantAPI.Services
             _context = context;
             _mapper = mapper;
         }
+        public DishDto GetById(int restaurantId, int dishId)
+        {
+            var restaurant = GetRestaurantById(restaurantId);
+
+            var dish = _context.Dishes
+                ?.SingleOrDefault(d => d.Id == dishId);
+
+            if(dish == null)
+                throw new NotFoundException("Dish not found");
+
+            return _mapper.Map<DishDto>(dish);
+        }
+
+        public IEnumerable<DishDto> GetAll(int restaurantId)
+        {
+            var restaurant = GetRestaurantById(restaurantId);
+
+            var dishes = _context.Dishes;
+
+            if(!dishes.Any())
+                throw new NotFoundException("Dishes not found");
+
+            return _mapper.Map<IEnumerable<DishDto>>(dishes);
+        }
+
         public int Create(int restaurantId, DishDto dishDto)
         {
-            var restaurant = _context
-                .Restaurants
-                .SingleOrDefault(x => x.Id == restaurantId);
-
-            if(restaurant == null)
-                throw new NotFoundException("Dish not found");
+            var restaurant = GetRestaurantById(restaurantId);
 
             var dish = _mapper.Map<Dish>(dishDto);
             dish.RestaurantId = restaurantId;
@@ -35,6 +60,26 @@ namespace RestaurantAPI.Services
             _context.SaveChanges();
 
             return dish.Id;
+        }
+
+        public void RemoveAll(int restaurantId)
+        {
+            var restaurant = GetRestaurantById(restaurantId);
+
+            _context.Remove(restaurant.Dishes);
+            _context.SaveChanges();
+        }
+
+        public Restaurant GetRestaurantById(int restaurantId)
+        {
+            var restaurant = _context
+                ?.Restaurants
+                ?.SingleOrDefault(x => x.Id == restaurantId);
+
+            if(restaurant == null)
+                throw new NotFoundException("Dish not found");
+
+            return restaurant;
         }
     }
 }
