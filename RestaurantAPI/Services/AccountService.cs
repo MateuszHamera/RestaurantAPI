10ca3b3bef0cdec334a3549a56bc8eda263b2554
Mateusz <mateusz.hamera@evatronix.com>
2022-09-16 09:03:41 +0200

    Fixed year formating in Claim.

--- RestaurantAPI/Services/AccountService.cs
+++ RestaurantAPI/Services/AccountService.cs
@@ -89,7 +89,7 @@ namespace RestaurantAPI.Services
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
                 new Claim(ClaimTypes.Name, $"{user.FirstName} {user.LastName}"),
                 new Claim(ClaimTypes.Role, $"{user.Role.Name}"),
-                new Claim("DateOfBirth", user.DateOfBirth.ToString("yyyyy-MM-dd"))
+                new Claim("DateOfBirth", user.DateOfBirth.ToString("yyyy-MM-dd"))
             };
 
             if(!string.IsNullOrEmpty(user.Nationality))
